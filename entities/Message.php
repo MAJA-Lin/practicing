e3c370ab0adb08bfc89ed6fd7d54f01ad81a11b1
scott_lin <typhoon31815@gmail.com>
2015-10-27 11:20:00 +0800

    新增entity宣告檔Message.php
    Add entity definition file Message.php (which connects to message_board.message)

---
+++ entities/Message.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @Entity @Table(name="message")
+ */
+class Message
+{
+    /**
+     * @Id @Column(type="integer") @GeneratedValue
+     */
+    protected $sn;
+    /**
+     * @Column(type="text")
+     */
+    protected $msg;
+    /**
+     * @Column(type="datetime")
+     */
+    protected $time;
+    /**
+     * @Column(type="string", length="20", nullable="TRUE")
+     */
+    protected $name;
+
+    public function getSN()
+    {
+        return $this->sn;
+    }
+
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
+    public function setMsg($msg)
+    {
+        $this->msg = $msg;
+    }
+
+    public function setTime()
+    {
+        $this->time = date("Y-m-d H:i:s");
+    }
+
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+}

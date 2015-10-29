7924746571929357c15b4ad867e809fb89a22d48
scott_lin <typhoon31815@gmail.com>
2015-10-29 12:20:25 +0800

    修改cli-config, 縮短行數與去掉不必要的引用
    Modify cli-config and delete unnecessary namespace use

--- board/cli-config.php
+++ board/cli-config.php
@@ -1,15 +1,6 @@
 <?php
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
-use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
-use Symfony\Component\Console\Helper\HelperSet;
 
-// replace with file to your own project bootstrap
 require_once 'bootstrap.php';
-
-$helperSet = new HelperSet(array(
-    'em' => new EntityManagerHelper($entityManager),
-    'conn' => new ConnectionHelper($entityManager->getConnection())
-));
 
 return ConsoleRunner::createHelperSet($entityManager);

49ac4ab8bf797f3f48d7866f5a8878340dfaa624
scott_lin <typhoon31815@gmail.com>
2015-11-27 18:11:15 +0800

    暫存, 下方有詳細記錄:
    Problem:
    1.
        沒有寫入datebase
        (record.id 顯示null)
    2.
        add頁面有時重整會跳到login 頁面, 且須重新登入
    3.
        session['customer']有時會消失; 比方說record/add頁面之後直接輸入網址到login,會被要求重新登入
    4.
        account在session跟database上都會更新(updateAccount版本)
        不額外new Account而使用session上存的$account的話,會出現index錯誤
        e.g. => Notice: Undefined index: 000000003b3ab2b400007f57d371cf1d
    5.
        ???

--- passbook/src/Scott/PassbookBundle/Controller/PassbookController.php
+++ passbook/src/Scott/PassbookBundle/Controller/PassbookController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Cookie;
 use Scott\PassbookBundle\Entity as Entity;
@@ -20,17 +21,147 @@ class PassbookController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $response = new Response;
         $session = $request->getSession();
         $customer = $session->get('customer');
 
         if (empty($customer) || is_null($customer)) {
             return $this->redirectToRoute('login');
-        } else {
-            return $this->render('ScottPassbookBundle:Passbook:index.html.twig', [
-                'request' => $customer,
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $account = $entityManager
+            ->getRepository('ScottPassbookBundle:Account')
+            ->findBy(['customer' => $customer[0]->getId()]);
+
+        $record = $entityManager
+            ->getRepository('ScottPassbookBundle:Record')
+            ->findBy(['account' => $account[0]->getId()]);
+
+        $session->set('Account', $account);
+        $session->set('Record', $record);
+
+        /*
+        $request->cookies->set('Account', $account);
+        $request->cookies->set('Record', $record);
+        //
+        $response->headers->setCookie(new Cookie('Account', $account));
+        $response->headers->setCookie(new Cookie('Record', $record));
+        */
+        $new_record = [];
+        $form = $this->createFormBuilder($new_record)
+            ->setMethod("POST")
+            ->setAction($this->generateUrl('record_add'))
+            ->add(
+                'option',
+                'choice', [
+                    'choices' => [
+                        'Save' => 'Save',
+                        'Withdraw' => 'Withdraw',
+                    ]
+                ])
+            ->add(
+                'amount',
+                'number', [
+                    'attr' => [
+                        'maxlength' => 12,
+                        'pattern' => '([0-9]*\.[0-9]+|[0-9]+)',
+                        'title' => 'Must be an integer or float',
+                    ]
+                ])
+            ->add(
+                'memo',
+                'text', [
+                    'attr' => [
+                        'maxlength' => 50
+                    ]
+                ])
+            ->add(
+                'signup',
+                'submit', [
+                    'label' => 'Add New Record'
+                ])
+            ->getForm();
+
+        return $this->render('ScottPassbookBundle:Passbook:index.html.twig', [
+            'form' => $form->createView(),
+            'request' => $request,
+            'response' => $response,
+        ]);
+
+
+    }
+
+    /**
+     * @Route("/reocrd/add", name="record_add")
+     * @Method("POST")
+     *
+     */
+    public function recordAddAction(Request $request)
+    {
+        //First: Add data into cookie
+        //Second: Add verification
+        //Update & retrive cookie
+        //將資料塞進seesion後, 如果要再寫入database, 是否要重新find(retrive from database)?
+
+        $form = $request->request->get('form');
+
+
+        $option =$form['option'];
+        $amount = (float) $form['amount'];
+        $memo = $form['memo'];
+
+        if (strlen($form['amount']) > 12) {
+
+            return $this->render('ScottPassbookBundle:Passbook:error.html.twig', [
+                'request' => $request,
+                'error' => "amount",
             ]);
         }
 
+        //Assume the value checker are done
+        $session = $request->getSession();
+        $account = $session->get('Account');
+
+        if (is_array($account)) {
+            $oldAccount = $account[0];
+        } else {
+            $oldAccount = $account;
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $record = new Entity\Record();
+        //$account = $request->session->get('Account');
+        $updateAccount = $entityManager
+            ->find('ScottPassbookBundle:Account', $oldAccount->getId());
+        $balance = $oldAccount->getBalance();
+
+        $record->setAccount($oldAccount);
+        $record->setBalance($balance);
+        $record->setCreateTime(new \DateTime());
+        $record->setMemo($memo);
+        $record->setAmount($amount);
+
+        if ($option == "Save") {
+            $oldAccount->setBalance($balance + $amount);
+        } else {
+            $oldAccount->setBalance($balance - $amount);
+        }
+
+        $entityManager->persist($oldAccount, $record);
+        $entityManager->flush();
+
+        $session->replace([
+            'Account' => $oldAccount,
+            'Record' => $record,
+        ]);
+
+        return $this->render('ScottPassbookBundle:Passbook:error.html.twig', [
+                'request' => $request,
+                'error' => "amount",
+                'value' => $record,
+            ]);
+        //return $this->redirectToRoute('index');
     }
 
 }
